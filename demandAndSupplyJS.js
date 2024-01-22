1d8de032aade2af41f29bd0e56e008e4ef3fc295
lolasanchezz <lolansanchez@icloud.com>
2024-01-22 14:25:22 -0500

    grid without numbers :)

--- demandAndSupplyJS.js
+++ demandAndSupplyJS.js
@@ -1,52 +1,105 @@
-
 var sceneWidth = 500;
 var sceneHeight = 500;
 var scaleX = 1;
 var scaleY = 1;
+var firstTime = true;
+console.log(firstTime);
 
 var stage = new Konva.Stage({
-  container: 'canvasContainer',   // id of container <div>
+  container: "canvasContainer", // id of container <div>
   width: sceneWidth,
   height: sceneHeight,
   id: "konvaStage",
 });
 var demAndSupLinesLayer = new Konva.Layer();
+var backgroundLayer = new Konva.Layer();
 
-
-function fitSceneIntoDiv (){
-  var container = document.getElementById('canvasContainer');
+function fitSceneIntoDiv() {
+  var container = document.getElementById("canvasContainer");
   var containerWidth = container.offsetWidth;
   var containerHeight = container.offsetHeight;
-  
-  var changeInScaleX = 500-(500-containerWidth);
-  var changeInScaleY = 500-(500-containerHeight);
+
+  var changeInScaleX = 500 - (500 - containerWidth);
+  var changeInScaleY = 500 - (500 - containerHeight);
   var scale = Math.min(changeInScaleX, changeInScaleY);
   return scale;
 }
-function checkDivHeightAndWidth (){
-var parentCanvasDiv = document.getElementById('canvasContainer');
-var divWidth = parentCanvasDiv.offsetWidth;
-var divHeight = parentCanvasDiv.offsetHeight;
-if ((divWidth <= 700) || (divHeight <=700)) {
-stage.width(fitSceneIntoDiv());
-stage.height(fitSceneIntoDiv());
-console.log(stage.width());
-console.log(stage.height());
+function checkDivHeightAndWidth() {
+  var parentCanvasDiv = document.getElementById("canvasContainer");
+  var divWidth = parentCanvasDiv.offsetWidth;
+  var divHeight = parentCanvasDiv.offsetHeight;
+  if (divWidth <= 500 || divHeight <= 500) {
+    stage.width(fitSceneIntoDiv());
+    stage.height(fitSceneIntoDiv());
+    console.log(stage.width());
+    console.log(stage.height());
+    var backgroundRectangleCanvas = document.getElementById(
+      "canvasContainerBackground"
+    );
+    backgroundRectangleCanvas.style.width = stage.width();
+    backgroundRectangleCanvas.style.height = stage.height();
+  }
 }
+function generateRect() {
+  
+  return new Konva.Rect({
+    x: xPos,
+    y: yPos,
+    fillEnabled: false,
+    width: squareWidth,
+    height: squareWidth,
+    stroke: "#cccccc",
+    strokeWidth: 0.5,
+  });
+  
 }
-window.addEventListener('resize', checkDivHeightAndWidth);
+
+window.addEventListener("resize", checkDivHeightAndWidth);
 
 var supLine = new Konva.Line({
-  points: [0,0,stage.width(),stage.height()],
-  stroke: 'black',
+  points: [0, 0, stage.width(), stage.height()],
+  stroke: "black",
   strokeWidth: 5,
+  listening: true,
 });
 var demLine = new Konva.Line({
-  points: [0, stage.height(), stage.width(),0],
-  stroke: 'black',
+  points: [0, stage.height(), stage.width(), 0],
+  stroke: "black",
   strokeWidth: 5,
-})
+  listening: true,
+});
+
+var equilibrium = new Konva.Circle({
+  x: stage.width() / 2,
+  y: stage.height() / 2,
+  radius: 10,
+  fill: "black",
+});
+demAndSupLinesLayer.add(equilibrium);
 demAndSupLinesLayer.add(supLine);
 demAndSupLinesLayer.add(demLine);
+backgroundLayer.add(generateRect());
+stage.add(backgroundLayer);
 stage.add(demAndSupLinesLayer);
 console.log("hii");
+
+
+var squareWidth = 20;
+var xPos = 0;
+var yPos = 0;
+
+
+
+
+
+while (yPos <= stage.height() - squareWidth) {
+  xPos = 0; // Reset xPos at the beginning of each row
+  while (xPos <= stage.width() - squareWidth) {
+    backgroundLayer.add(generateRect());
+   xPos = xPos + squareWidth;
+  }
+  yPos = yPos + squareWidth;
+}
+stage.add(backgroundLayer);
+
+
